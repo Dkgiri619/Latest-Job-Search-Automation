ae7693875bef040713fbf88587d532c98515bc97
Dkgiri619 <dkgiri619@gmail.com>
2021-06-01 14:05:17 +0530

    glassdoor data successful

--- login.js
+++ login.js
@@ -33,6 +33,8 @@ let jobsData1 = [];
     await tab.keyboard.press("A");
     await tab.keyboard.up("Control");
     await tab.type("#sc\\.location", place);
+    await tab.waitForSelector(".autocomplete-suggestions span"); // wait for suggestion
+    await tab.click(".autocomplete-suggestions span");
     await tab.click("button[type='submit']");
     await tab.waitForSelector(".jobLink.eigr9kq2");
     let allJobTags = await tab.$$(".jobLink.eigr9kq2");
@@ -42,35 +44,48 @@ let jobsData1 = [];
         oneLink= "https://www.glassdoor.co.in"+oneLink;
         allJobLinks.push(oneLink);
     }
-    await getJobData(allJobLinks[0],browser);
-    // for(let i=0;i<allJobLinks.length;i++){
-    //     await getJobData(allJobLinks[i], browser);
-    //     ///console.log(allJobLinks[i]);
-    // }
+    //await getJobData(allJobLinks[3],browser);
+    for(let i=0;i<allJobLinks.length;i++){
+        let newTab = await browser.newPage();
+        await getJobData(allJobLinks[i], newTab);
+    }
 
 })();
 
-async function getJobData(link, browser){
-    let jobData = { company: '', designation: '', location: '', salary: '', link: ''};
-    let tab = await browser.newPage();
+async function getJobData(link, tab){
+    let jobData = { "company": '', "designation": '', "location": '', "salary": '', "link": ''};
     await tab.goto(link);
     // company name
     await tab.waitForSelector(".css-16nw49e.e11nt52q1");
     let nameTag = await tab.$(".css-16nw49e.e11nt52q1");
     let name = await tab.evaluate(function(elem){return elem.textContent},nameTag);
     let fName = name.slice(0, name.length-4);
-    jobData[company]=fname;
+    jobData["company"]=fName;
     // location 
     let locTag = await tab.$(".css-1v5elnn.e11nt52q2");
     let loc = await tab.evaluate(function(elem){return elem.textContent},locTag);
-    jobData[location] = loc;
+    jobData["location"] = loc;
     // designation
     let postTag  = await tab.$(".css-17x2pwl.e11nt52q6");
     let post = await tab.evaluate(function(elem){return elem.textContent},postTag);
-    jobData[designation] = post;
+    jobData["designation"] = post;
     // salary
-    const isSalaryPresent = await tab.$('.css-1v5elnn.e11nt52q2 .small'). then (res =>!! res);
-    if(isSalaryPresent){
-        let price = tab.evaluate(function(elem){}, isSalaryPresent)
+    const isSalaryPresent = await tab.$('.css-1v5elnn.e11nt52q2 .small')
+    if(isSalaryPresent!=null){
+        let price = await tab.evaluate(function(elem){return elem.textContent}, isSalaryPresent);
+        jobData["salary"]=price;
+    }else jobData["salary"]="not specified";
+    // job link
+    const isJobPresent = await tab.$('.gd-ui-button.applyButton.e1ulk49s0.css-1m0gkmt')
+    if(isJobPresent!=null){
+        let joblink = await tab.evaluate(function(elem){return elem.getAttribute("href")}, isJobPresent);
+        joblink = "https://www.glassdoor.co.in" + joblink;
+        jobData["link"]=joblink;
+    }else {
+        let linkTag = await tab.$(".indeed-apply-status-not-applied");
+        let joblink = await tab.evaluate(function(elem){return elem.dataset.indeedApplyJoburl},linkTag);
+        jobData["link"]=joblink;
     }
+    console.log(jobData);
+    tab.close();
 }
